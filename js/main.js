a05663af4c1e8f0b78afa16f4c4ef174fba3f9d7
dinhthanhnam <149922885+dinhthanhnam@users.noreply.github.com>
2023-12-30 15:43:29 +0700

    semi final

--- js/main.js
+++ js/main.js
@@ -15,49 +15,49 @@ const products = [
   {
     id: 2,
     name: "ASUS TUF FX705DT-H7138T Gaming Laptop",
-    price: "42.600.000",
+    price: "42.600.000₫",
     type: "asus",
   },
   {
     id: 3,
     name: "Acer Swift 7 SF714-52T-7134 Laptop Black",
-    price: "49.600.000",
+    price: "49.600.000₫",
     type: "acer",
   },
   {
     id: 4,
     name: "ASUS ROG Strix SCAR III G531GN-VAZ160T Laptop",
-    price: "44.600.000",
+    price: "44.600.000₫",
     type: "asus",
   },
   {
     id: 5,
     name: "ASUS ProArt StudioBook Pro 17 W700G1T-AV046T Laptop",
-    price: "45.600.000",
+    price: "45.600.000₫",
     type: "asus",
   },
   {
     id: 6,
     name: "LG Gram 17Z90N-V.AH75A5 Laptop Silver",
-    price: "39.600.000",
+    price: "39.600.000₫",
     type: "LG",
   },
   {
     id: 7,
     name: "HP Omen 15-dh0172TX Gaming Laptop",
-    price: "44.600.000",
+    price: "44.600.000₫",
     type: "HP",
   },
   {
     id: 8,
     name: "ASUS VivoBook 15 A512FA-EJ1281T Laptop",
-    price: "15.600.000",
+    price: "15.600.000₫",
     type: "asus",
   },
   {
     id: 9,
     name: "Acer Predator Helios 300 PH315-52-78HH Gaming Laptop",
-    price: "35.600.000",
+    price: "35.600.000₫",
     type: "Acer",
   },
   {
@@ -99,52 +99,70 @@ const products = [
   {
     id: 16,
     name: "Laptop MSI Modern 15 B12MO-487VN ",
-    price: "16.990.000",
+    price: "16.990.000₫",
     type: "MSI",
   },
   {
     id: 17,
     name: "Laptop Gaming Acer Nitro V ANV15-51-55CA NH.QN8SV.004",
-    price: "26.990.000 ",
+    price: "26.990.000₫",
     type: "acer",
   },
   {
     id: 18,
     name: "Laptop Gaming Acer Aspire 7 A715-76G-5806 - NH.QMFSV.002",
-    price: "18.590.000",
+    price: "18.590.000₫",
     type: "acer",
   },
   {
     id: 19,
     name: "Laptop Gaming Acer Nitro 5 Tiger AN515-58-52SP NH.QFHSV.001",
-    price: "21.490.000",
+    price: "21.490.000₫",
     type: "acer",
   },
   {
     id: 20,
     name: "Laptop HP 14S-dq5100TU 7C0Q0PA",
-    price: "13.490.000",
+    price: "13.490.000₫",
     type: "HP",
   },
   {
     id: 21,
     name: "Laptop HP Pavilion 15-eg3093TU 8C5L4PA",
-    price: "17.990.000",
+    price: "17.990.000₫",
     type: "HP",
   },
   {
     id: 22,
     name: "Laptop HP ENVY X360 13-bf0090TU 76B13PA",
-    price: "27.490.000",
+    price: "27.490.000₫",
     type: "HP",
   },
   {
     id: 23,
     name: "Laptop Asus TUF Gaming F15 FX507ZC4-HN074W",
-    price: "19.490.000",
+    price: "19.490.000₫",
     type: "asus",
   },
 ];
+
+// Banner
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.banner-image');
+
+function showImage(index) {
+  for (let i = 0; i < images.length; i++) {
+    images[i].style.display = 'none';
+  }
+  images[index].style.display = 'block';
+}
+
+function nextImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  showImage(currentImageIndex);
+}
+
+setInterval(nextImage, 3000);
 
 // New Products
 products.forEach((product, index) => {
@@ -152,18 +170,13 @@ products.forEach((product, index) => {
     newProducts
       ? (newProducts.innerHTML += `
         <li class="new-product">
-            <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+            <a href="products/NewProducts/product${product.id}.html">
                 <div class="product-img">
-                    <img src="/img/product/${index + 1}.jpg" alt="" />
+                    <img src="img/product/${index + 1}.jpg" alt="" />
                 </div>
                 <div class="product-info">
-                    <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-                      product.name
-                    }</h3>
-                    <p 
-                    style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-                    ${product.price}
-                    </p>
+                    <h3>${product.name}</h3>
+                    <p>${product.price}</p>
                 </div>
             </a>
         </li>
@@ -171,24 +184,21 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
+
 // Hot Products
 products.forEach((product, index) => {
   if (index <= 9) {
     hotProducts
       ? (hotProducts.innerHTML += `
       <li class="product">
-        <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+        <a href="products/HotProducts/product${product.id}.html">
           <div class="product-img">
-              <img src="/img/product/${index + 1}.jpg" alt="" />
+              <img src="img/product/${index + 1}.jpg" alt="" />
           </div>
           <div class="product-info">
-              <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-                product.name
-              }</h3>
-              <p 
-              style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-              ${product.price}
-              </p>
+              <h3>${product.name}</h3>
+              <p>${product.price}</p>
           </div>
         </a>
       </li>
@@ -196,6 +206,7 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
 // Asus Products
 products.forEach((product, index) => {
   if (index <= 25 && product.type === "asus") {
@@ -203,18 +214,13 @@ products.forEach((product, index) => {
     asusProducts
       ? (asusProducts.innerHTML += `
       <li class="product">
-      <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+      <a href="products/asus/product${product.id}.html">
         <div class="product-img">
-            <img src="/img/product/${index + 1}.jpg" alt="" />
+            <img src="img/product/${index + 1}.jpg" alt="" />
         </div>
         <div class="product-info">
-            <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-              product.name
-            }</h3>
-            <p 
-            style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-            ${product.price}
-            </p>
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
         </div>
       </a>
     </li>
@@ -222,6 +228,7 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
 // Acer Products
 products.forEach((product, index) => {
   if (index <= 20 && product.type === "acer") {
@@ -229,18 +236,13 @@ products.forEach((product, index) => {
     acerProducts
       ? (acerProducts.innerHTML += `
       <li class="product">
-      <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+      <a href="products/acer/product${product.id}.html">
         <div class="product-img">
-            <img src="/img/product/${index + 1}.jpg" alt="" />
+            <img src="img/product/${index + 1}.jpg" alt="" />
         </div>
         <div class="product-info">
-            <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-              product.name
-            }</h3>
-            <p 
-            style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-            ${product.price}
-            </p>
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
         </div>
       </a>
     </li>
@@ -248,6 +250,7 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
 // HP Products
 products.forEach((product, index) => {
   if (index <= 25 && product.type === "HP") {
@@ -255,18 +258,13 @@ products.forEach((product, index) => {
     hpProducts
       ? (hpProducts.innerHTML += `
       <li class="product">
-      <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+      <a href="products/HP/product${product.id}.html">
         <div class="product-img">
-            <img src="/img/product/${index + 1}.jpg" alt="" />
+            <img src="img/product/${index + 1}.jpg" alt="" />
         </div>
         <div class="product-info">
-            <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-              product.name
-            }</h3>
-            <p 
-            style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-            ${product.price}
-            </p>
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
         </div>
       </a>
     </li>
@@ -274,6 +272,7 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
 // LG Products
 products.forEach((product, index) => {
   if (index <= 12 && product.type === "LG") {
@@ -281,18 +280,13 @@ products.forEach((product, index) => {
     lgProducts
       ? (lgProducts.innerHTML += `
       <li class="product">
-      <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+      <a href="products/LG/product${product.id}.html">
         <div class="product-img">
-            <img src="/img/product/${index + 1}.jpg" alt="" />
+            <img src="img/product/${index + 1}.jpg" alt="" />
         </div>
         <div class="product-info">
-            <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-              product.name
-            }</h3>
-            <p 
-            style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-            ${product.price}
-            </p>
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
         </div>
       </a>
     </li>
@@ -300,6 +294,7 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
 // MSI Products
 products.forEach((product, index) => {
   if (index <= 20 && product.type === "MSI") {
@@ -307,18 +302,13 @@ products.forEach((product, index) => {
     msiProducts
       ? (msiProducts.innerHTML += `
       <li class="product">
-      <a href="https://gearvn.com/products/laptop-asus-vivobook-14x-oled-s3405va-km071w">
+      <a href="products/MSI/product${product.id}.html">
         <div class="product-img">
-            <img src="/img/product/${index + 1}.jpg" alt="" />
+            <img src="img/product/${index + 1}.jpg" alt="" />
         </div>
         <div class="product-info">
-            <h3 style="margin-bottom: 15px; text-overflow: ellipsis;">${
-              product.name
-            }</h3>
-            <p 
-            style= "color: #e40000 !important; font-weight: bold;font-size: 15px">
-            ${product.price}
-            </p>
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
         </div>
       </a>
     </li>
@@ -326,3 +316,37 @@ products.forEach((product, index) => {
       : "";
   }
 });
+
+// Image
+const thumbnails = document.querySelectorAll('.thumbnail');
+thumbnails.forEach(thumbnail => {
+    thumbnail.addEventListener('click', function() {
+        const imageSrc = this.src;
+        const mainImage = document.querySelector('.main-image');
+        mainImage.src = imageSrc;
+    });
+});
+const mainImage = document.querySelector('.main-image');
+const fullscreenImageContainer = document.querySelector('.fullscreen-image-container');
+const fullscreenImage = document.querySelector('.fullscreen-image');
+const closeBtn = document.querySelector('.close-btn');
+mainImage.addEventListener('click', function() {
+    fullscreenImage.src = this.src;
+    fullscreenImageContainer.style.display = 'flex';
+});
+closeBtn.addEventListener('click', function() {
+    fullscreenImageContainer.style.display = 'none';
+});
+
+// Pop-up
+document.querySelector('.add-to-cart').addEventListener('click', function() {
+    document.getElementById('popupContainer').style.display = 'block';
+    document.querySelector('.popup-content').textContent = 'Sản phẩm đã được thêm vào giỏ hàng.';
+});
+document.querySelector('.checkout').addEventListener('click', function() {
+    document.getElementById('popupContainer').style.display = 'block';
+    document.querySelector('.popup-content').textContent = 'Chuyển đến trang thanh toán.';
+});
+function closePopup() {
+    document.getElementById('popupContainer').style.display = 'none';
+}
