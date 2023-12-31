1e6700bda761366aef558540fa73448ba3dcac4c
dinhthanhnam <149922885+dinhthanhnam@users.noreply.github.com>
2023-12-31 22:47:20 +0700

    final

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+const btn = document.querySelectorAll(".button");
+const login = document.querySelector(".login");
+const register = document.querySelector(".registration");
+const loginSuccess = document.querySelector(".login-success");
+const checkBtn = document.querySelector("#check");
+btn.forEach((btn) => {
+  btn.addEventListener("click", () => {});
+});
+let action = "login";
+const handleCheck = () => {
+  if (action === "login") {
+    action = "register";
+    login.style.display = "none";
+    register.style.display = "block";
+  } else {
+    action = "login";
+    login.style.display = "block";
+    register.style.display = "none";
+  }
+};
+const successHandler = () => {
+  loginSuccess.style.display = "block";
+  login.style.display = "none";
+  register.style.display = "none";
+  if (action === "login") {
+    loginSuccess.innerHTML += `<h1>Thành công</h1>
+    <p>Chúng tôi đã nhận được yêu cầu của bạn;<br /> chúng tôi sẽ xử lí ngay</p>`;
+  }
+  if (action === "register") {
+    loginSuccess.innerHTML += `<h1>Thành công</h1>
+    <p>Chúng tôi đã nhận được yêu cầu của bạn;<br /> chúng tôi sẽ xử lí ngay</p>
+    `;
+  }
+};
 const newProducts = document.querySelector(".new-products");
 const hotProducts = document.querySelector(".hot-products");
 const asusProducts = document.querySelector(".asus-products");
@@ -148,13 +182,13 @@ const products = [
 
 // Banner
 let currentImageIndex = 0;
-const images = document.querySelectorAll('.banner-image');
+const images = document.querySelectorAll(".banner-image");
 
 function showImage(index) {
   for (let i = 0; i < images.length; i++) {
-    images[i].style.display = 'none';
+    images[i].style.display = "none";
   }
-  images[index].style.display = 'block';
+  images[index].style.display = "block";
 }
 
 function nextImage() {
@@ -162,7 +196,7 @@ function nextImage() {
   showImage(currentImageIndex);
 }
 
-setInterval(nextImage, 5000);
+setInterval(nextImage, 3000);
 
 // New Products
 products.forEach((product, index) => {
@@ -184,7 +218,6 @@ products.forEach((product, index) => {
       : "";
   }
 });
-
 
 // Hot Products
 products.forEach((product, index) => {
@@ -318,35 +351,39 @@ products.forEach((product, index) => {
 });
 
 // Image
-const thumbnails = document.querySelectorAll('.thumbnail');
-thumbnails.forEach(thumbnail => {
-    thumbnail.addEventListener('click', function() {
-        const imageSrc = this.src;
-        const mainImage = document.querySelector('.main-image');
-        mainImage.src = imageSrc;
-    });
+const thumbnails = document.querySelectorAll(".thumbnail");
+thumbnails.forEach((thumbnail) => {
+  thumbnail.addEventListener("click", function () {
+    const imageSrc = this.src;
+    const mainImage = document.querySelector(".main-image");
+    mainImage.src = imageSrc;
+  });
 });
-const mainImage = document.querySelector('.main-image');
-const fullscreenImageContainer = document.querySelector('.fullscreen-image-container');
-const fullscreenImage = document.querySelector('.fullscreen-image');
-const closeBtn = document.querySelector('.close-btn');
-mainImage.addEventListener('click', function() {
-    fullscreenImage.src = this.src;
-    fullscreenImageContainer.style.display = 'flex';
+const mainImage = document.querySelector(".main-image");
+const fullscreenImageContainer = document.querySelector(
+  ".fullscreen-image-container"
+);
+const fullscreenImage = document.querySelector(".fullscreen-image");
+const closeBtn = document.querySelector(".close-btn");
+mainImage.addEventListener("click", function () {
+  fullscreenImage.src = this.src;
+  fullscreenImageContainer.style.display = "flex";
 });
-closeBtn.addEventListener('click', function() {
-    fullscreenImageContainer.style.display = 'none';
+closeBtn.addEventListener("click", function () {
+  fullscreenImageContainer.style.display = "none";
 });
 
 // Pop-up
-document.querySelector('.add-to-cart').addEventListener('click', function() {
-    document.getElementById('popupContainer').style.display = 'block';
-    document.querySelector('.popup-content').textContent = 'Sản phẩm đã được thêm vào giỏ hàng.';
+document.querySelector(".add-to-cart").addEventListener("click", function () {
+  document.getElementById("popupContainer").style.display = "block";
+  document.querySelector(".popup-content").textContent =
+    "Sản phẩm đã được thêm vào giỏ hàng.";
 });
-document.querySelector('.checkout').addEventListener('click', function() {
-    document.getElementById('popupContainer').style.display = 'block';
-    document.querySelector('.popup-content').textContent = 'Chuyển đến trang thanh toán.';
+document.querySelector(".checkout").addEventListener("click", function () {
+  document.getElementById("popupContainer").style.display = "block";
+  document.querySelector(".popup-content").textContent =
+    "Chuyển đến trang thanh toán.";
 });
 function closePopup() {
-    document.getElementById('popupContainer').style.display = 'none';
+  document.getElementById("popupContainer").style.display = "none";
 }
